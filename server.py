a3bd0fe3183364bb72a5d48feab5579b9c147c4f
iamPres <prestowillis@gmail.com>
2022-09-25 00:55:07 -0500

    added files

---
+++ server.py
@@ -0,0 +1,42 @@
+from predict import predict
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from flask import Flask, request
+import numpy as np
+import base64
+from PIL import Image
+import io
+import mediapipe as mp
+import tensorflow as tf
+
+app = Flask(__name__)
+
+def get_model():
+    return tf.keras.models.load_model("model.h5")
+
+model = get_model()
+mpFaceMesh = mp.solutions.face_mesh
+faceMesh = mpFaceMesh.FaceMesh(max_num_faces=1)
+
+@app.route("/prediction", methods=['POST'])
+def get_prediction():
+    photo = request.get_json()['photo']
+    imgdata = base64.b64decode(photo)
+    img = Image.open(io.BytesIO(imgdata))
+    img = np.array(img) 
+    img = img[:, :, ::-1].copy() 
+    result = 0
+    try:
+        result = predict(img, model)
+    except:
+        result = -1
+    return {"Output": result}
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+    server_address = ('localhost', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    run()
